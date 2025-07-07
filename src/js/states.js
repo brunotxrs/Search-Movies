5be30724622e4bf7b6d33a22653f2505f8898817
brunotxrs <183097017+brunotxrs@users.noreply.github.com>
2025-07-07 17:44:48 -0400

    feat: by adding the html points of the LOGIN component, these points will be manipulated

--- src/js/states.js
+++ src/js/states.js
@@ -3,4 +3,14 @@ export const states = {
     button_preload: document.getElementById('button'),
     class_hidden: 'hidden',
     spinner: document.querySelector('.spinner'),
+    login: document.querySelector('.login'),
+    ico_close: document.getElementById('ico_close'),
+    ico_open: document.getElementById('ico_open'),
+    i_name: document.getElementById('i_name'),
+    i_password: document.getElementById('i_password'),
+    ico_off: document.getElementById('ico_off'),
+    ico_on: document.getElementById('ico_on'),
+    i_button_login: document.getElementById('i_button_login'),
+    msg_error:document.getElementById('msg_error'),
+
 }
