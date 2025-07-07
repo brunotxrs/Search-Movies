1b4e0fa06c4dcacdbf2fa6e65c30c2f7c5425cb5
brunotxrs <183097017+brunotxrs@users.noreply.github.com>
2025-07-07 10:40:30 -0400

    feat: create the module [states] for save points the html for manipulation

---
+++ src/js/states.js
@@ -0,0 +1,6 @@
+export const states = {
+    preload: document.querySelector('.preload'),
+    button_preload: document.getElementById('button'),
+    class_hidden: 'hidden',
+    spinner: document.querySelector('.spinner'),
+}
