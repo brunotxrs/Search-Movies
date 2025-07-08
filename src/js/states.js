0c01fb143d147b99af2ad68ed0ef453caff88123
brunotxrs <183097017+brunotxrs@users.noreply.github.com>
2025-07-08 17:58:51 -0400

    feat: updated the elements of the stats module

--- src/js/states.js
+++ src/js/states.js
@@ -1,16 +1,7 @@
 export const states = {
     preload: document.querySelector('.preload'),
-    button_preload: document.getElementById('button'),
     class_hidden: 'hidden',
     spinner: document.querySelector('.spinner'),
     login: document.querySelector('.login'),
-    ico_close: document.getElementById('ico_close'),
-    ico_open: document.getElementById('ico_open'),
-    i_name: document.getElementById('i_name'),
-    i_password: document.getElementById('i_password'),
-    ico_off: document.getElementById('ico_off'),
-    ico_on: document.getElementById('ico_on'),
-    i_button_login: document.getElementById('i_button_login'),
-    msg_error:document.getElementById('msg_error'),
-
+    registration_password_recovery: document.querySelector('.registration_password_recovery'),
 }
