fb4f4ca8cd7f3c5f19e35aeb904c8e5d4d88882c
brunotxrs <183097017+brunotxrs@users.noreply.github.com>
2025-07-08 18:06:37 -0400

    feat: import only one module the preload

--- src/js/initialize_app.js
+++ src/js/initialize_app.js
@@ -1,17 +1,7 @@
-import {  states  } from './states.js';
-import {  login  } from './login.js';
+
+import {  preload } from './preload.js';
+
 
 export function initializeApp(){
-    
-    states.button_preload.addEventListener('click', () => {
-
-        states.preload.classList.add(states.class_hidden)
-        states.spinner.classList.remove(states.class_hidden);
-        setTimeout(() => {
-            states.spinner.classList.add(states.class_hidden);
-            login()
-        }, 1000)
-    })
-
-    
+    preload()
 }
