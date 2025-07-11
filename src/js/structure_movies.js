6b02b02952389ebc072d8e9f447e36c82bcf05a5
brunotxrs <183097017+brunotxrs@users.noreply.github.com>
2025-07-11 16:33:45 -0400

    feat: update the structure

--- src/js/structure_movies.js
+++ src/js/structure_movies.js
@@ -10,7 +10,7 @@ export function htmlMovie() {
 
         <div class="container_display_area">
 
-            <div class="box_search">
+            <div class="box_search" id="search_box">
 
                 <div class="box_user">
                     <div class="box_img">
@@ -25,17 +25,14 @@ export function htmlMovie() {
 
                 </div>
 
-                <form>
-                    <label for="i_search" class="label">
-                        <svg class="ico_search" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
+                <label for="i_search" class="label">
+                    <svg class="ico_search" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
 
-                        <input type="text" placeholder="Search" autocomplete="off" id="i_search" class="input">
-                    </label>
-                </form>
-
+                    <input type="text" placeholder="Search" autocomplete="off" id="input_search" class="input">
+                </label>
             </div>
 
-            <div class="cards">
+            <div class="cards" id="container_cards">
                 <div class="coming_soon">
                     <span class="sub_title">Em breve</span>
 
@@ -60,9 +57,9 @@ export function htmlMovie() {
 
             </div>
 
-            <div class="selection_types">
-                <span id="series">Series</span>
+            <div class="selection_types" id='types_selections'>
                 <span id="movies">Filmes</span>
+                <span id="series">Series</span>
                 <span id="my_list">Minha Lista</span>
             </div>
 
