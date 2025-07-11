49b6ba29a1bbac795674dadd207b2274ab0b7757
brunotxrs <183097017+brunotxrs@users.noreply.github.com>
2025-07-10 21:44:49 -0400

    feat: add new element for manipulation

--- src/js/states.js
+++ src/js/states.js
@@ -4,4 +4,5 @@ export const states = {
     spinner: document.querySelector('.spinner'),
     login: document.querySelector('.login'),
     registration_password_recovery: document.querySelector('.registration_password_recovery'),
+    display_area: document.querySelector('.display_area'),
 }
