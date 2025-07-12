fb4ad2fb4f04b5d4ba71a6a3f34a36066c45e5e7
brunotxrs <183097017+brunotxrs@users.noreply.github.com>
2025-07-12 10:57:01 -0400

    feat: import new module the moduleGenresTypes.js

--- src/js/moviesScreens.js
+++ src/js/moviesScreens.js
@@ -1,9 +1,11 @@
 import { moviePoster } from './modulePosterMovie.js';
-import {  searchMovie  } from './moduleSearchMovies.js'
+import {  searchMovie  } from './moduleSearchMovies.js';
 import {  trendingNow  } from './moduleTrendingNow.js';
+import {  typesGenres  } from './moduleGenresTypes.js';
 
 export function initializePageMovies(){
     trendingNow()
     moviePoster()
+    typesGenres()
     searchMovie()
 }
