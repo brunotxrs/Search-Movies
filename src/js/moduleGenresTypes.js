e4855e3cf0e67474a085644a6ffed946e0eb6a65
brunotxrs <183097017+brunotxrs@users.noreply.github.com>
2025-07-13 08:25:35 -0400

    feat: update the functions for create types of genres dinamic by js

--- src/js/moduleGenresTypes.js
+++ src/js/moduleGenresTypes.js
@@ -1,31 +1,45 @@
 import {  trendingNow  } from './moduleTrendingNow.js';
+import {  genresPromise } from './fetch_api.js';
 
-const stateTypesGenres = {
-    all: document.getElementById('all'),
-    action: document.getElementById('action'),
-    adventure: document.getElementById('adventure'),
-    animation: document.getElementById('animation'),
-    comedy: document.getElementById('comedy'),
-    crime: document.getElementById('crime'),
-    documentary: document.getElementById('documentary'),
-    drama: document.getElementById('drama'),
-    family: document.getElementById('family'),
-    fantasy: document.getElementById('fantasy'),
-    history: document.getElementById('history'),
-    horror: document.getElementById('horror'),
-    music: document.getElementById('music'),
-    mystery: document.getElementById('mystery'),
-    romance: document.getElementById('romance'),
-    'science-fiction': document.getElementById('science-fiction'),
-    'tv-movie': document.getElementById('tv-movie'),
-    thriller: document.getElementById('thriller'),
-    war: document.getElementById('war'),
-    western: document.getElementById('western'),
-    cards: document.getElementById('cards'),
+let selectionType = ''
+
+const typesType = document.querySelector('.types_type')
+typesType.innerHTML = ''
+
+async function createElementsGenres() {
+    const genreMap = await genresPromise;
+    const spanFirst = document.createElement('span');
+    spanFirst.setAttribute('id', 'all')
+    spanFirst.innerHTML = 'All'
+    typesType.appendChild(spanFirst)
+    spanFirst.addEventListener('click', () => {
+        handleGenreButtonClick('all');
+    });
+
+
+    genreMap.genres.forEach((e, i) => {
+        const nameGenres = e.name
+        const spanGenres = document.createElement('span')
+        spanGenres.innerHTML = `${e.name}`;
+        spanGenres.setAttribute('id', e.name.toLowerCase().replace(/\s/g, '-'))
+        typesType.appendChild(spanGenres) 
+
+        spanGenres.addEventListener('click', () => {
+            selectionType = nameGenres
+            handleGenreButtonClick(selectionType)
+        })
+    })
+
+
 
 }
 
-let selectionType = ''
+createElementsGenres()
+
+const stateTypesGenres = {
+    cards: document.getElementById('cards'),
+}
+
 
 const spinnerLoad = document.createElement('span')
 spinnerLoad.setAttribute('class', 'load_spinner')
@@ -41,94 +55,14 @@ function stateTypeOfContainerCards(value){
     }
 }
 
-stateTypesGenres.all.addEventListener('click', () => {
-    handleGenreButtonClick('all');
-});
-
-stateTypesGenres.action.addEventListener('click', () => {
-    handleGenreButtonClick('Ação');
-});
-
-stateTypesGenres.adventure.addEventListener('click', () => {
-    handleGenreButtonClick('Aventura');
-});
-
-stateTypesGenres.animation.addEventListener('click', () => {
-    handleGenreButtonClick('Animação');
-});
-
-stateTypesGenres.comedy.addEventListener('click', () => {
-    handleGenreButtonClick('Comédia');
-});
-
-stateTypesGenres.crime.addEventListener('click', () => {
-    handleGenreButtonClick('Crime');
-});
-
-stateTypesGenres.documentary.addEventListener('click', () => {
-    handleGenreButtonClick('Documentário');
-});
-
-stateTypesGenres.drama.addEventListener('click', () => {
-    handleGenreButtonClick('Drama');
-});
-
-stateTypesGenres.family.addEventListener('click', () => {
-    handleGenreButtonClick('Família');
-});
-
-stateTypesGenres.fantasy.addEventListener('click', () => {
-    handleGenreButtonClick('Fantasia');
-});
-
-stateTypesGenres.history.addEventListener('click', () => {
-    handleGenreButtonClick('História');
-});
-
-stateTypesGenres.horror.addEventListener('click', () => {
-    handleGenreButtonClick('Terror');
-});
-
-stateTypesGenres.music.addEventListener('click', () => {
-    handleGenreButtonClick('Música');
-});
-
-stateTypesGenres.mystery.addEventListener('click', () => {
-    handleGenreButtonClick('Mistério');
-});
-
-stateTypesGenres.romance.addEventListener('click', () => { 
-    handleGenreButtonClick('Romance');
-});
-
-stateTypesGenres['science-fiction'].addEventListener('click', () => {
-    handleGenreButtonClick('Ficção científica');
-});
-
-stateTypesGenres['tv-movie'].addEventListener('click', () => {
-    handleGenreButtonClick('Cinema TV');
-});
-
-stateTypesGenres.thriller.addEventListener('click', () => { 
-    handleGenreButtonClick('Thriller');
-});
-
-stateTypesGenres.war.addEventListener('click', () => { 
-    handleGenreButtonClick('Guerra');
-});
-
-stateTypesGenres.western.addEventListener('click', () => {
-    handleGenreButtonClick('Faroeste');
-});
-
 function handleGenreButtonClick(genreName) {
     selectionType = genreName;
     stateTypeOfContainerCards(false); // Inicia o spinner
     
-    setTimeout(async() => {    
+    setTimeout(async() => {
         stateTypeOfContainerCards(true); // Remove o spinner
         await trendingNow(selectionType);
-    }, 1000);
+    }, 3000);
 
 }
 
