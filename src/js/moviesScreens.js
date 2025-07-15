6f07f226872625f0d1e276542bf3d060ff6a5f92
brunotxrs <183097017+brunotxrs@users.noreply.github.com>
2025-07-15 11:57:52 -0400

    feat: import new module the selection

--- src/js/moviesScreens.js
+++ src/js/moviesScreens.js
@@ -2,10 +2,12 @@ import { moviePoster } from './modulePosterMovie.js';
 import {  searchMovie  } from './moduleSearchMovies.js';
 import {  trendingNow  } from './moduleTrendingNow.js';
 import {  typesGenres  } from './moduleGenresTypes.js';
+import {  selection  } from './moduleSelections.js'
 
 export function initializePageMovies(){
-    trendingNow()
-    moviePoster()
-    typesGenres()
-    searchMovie()
-}
+    selection();
+    trendingNow();
+    moviePoster();
+    typesGenres();
+    searchMovie();
+};
