742799704098ee935e7af0cf6b7578b395daa8dc
brunotxrs <183097017+brunotxrs@users.noreply.github.com>
2025-07-16 22:09:41 -0400

    feat: create this module for apply trailers after recept id of movies

---
+++ src/js/moduleTrailersMovies.js
@@ -0,0 +1,109 @@
+import { fetchMovieTrailers  } from './fetch_api.js';
+
+
+
+export async function trailersMovies(idMovies) {
+    
+    try {
+        const trailerMovie = await fetchMovieTrailers(idMovies, 'pt-BR');
+        const boxMovie = document.querySelector('.box-img');
+        
+        
+        if (trailerMovie) {
+            
+            // A 'key' ID do vídeo no YouTube.
+            const videoKey = trailerMovie.key;
+            // const videoName = trailerMovie.name; 
+
+            // para construir a URL do YouTube para incorporar (iframe)
+            const youtubeEmbedUrl = `https://www.youtube.com/embed/${videoKey}?autoplay=1&rel=0`; // autoplay para iniciar automaticamente, rel=0 para não mostrar vídeos relacionados de outros canais
+            // const youtubeWatchUrl = `https://www.youtube.com/watch?v=${videoKey}`;
+
+            // limpando a imagem
+            const imageMovie = document.getElementById('imageMovie')
+            imageMovie.style.display = 'none'
+
+            if (boxMovie) {
+                const iframe = document.createElement('iframe');
+                iframe.src = youtubeEmbedUrl;
+                iframe.width = "100%";
+                iframe.height = "100%";
+                iframe.setAttribute("frameborder", "0");
+                iframe.setAttribute("allowfullscreen", "");
+                iframe.setAttribute("allow", "autoplay; encrypted-media");
+                
+                boxMovie.appendChild(iframe);
+            }
+
+        } 
+
+        // for apply message for no have trailers 
+        if(trailerMovie === null){
+
+            const pInfo = document.createElement('p');
+            pInfo.textContent = 'Sorry no have Trailers';
+            pInfo.classList.add('info_no_trailers')
+            boxMovie.appendChild(pInfo)
+
+        }
+
+
+    } catch (error) {
+        console.error('Erro ao buscar trailers do filme:', error);
+    }
+}
+
+export async function trailersForPoster(idMovies) {
+    
+    try {
+        const trailerMovie = await fetchMovieTrailers(idMovies, 'pt-BR');
+        const posterTrailer = document.querySelector('.coming_soon')
+        
+        
+        if (trailerMovie) {
+            
+            // A 'key' ID do vídeo no YouTube.
+            const videoKey = trailerMovie.key;
+            // const videoName = trailerMovie.name; 
+
+            // para construir a URL do YouTube para incorporar (iframe)
+            const youtubeEmbedUrl = `https://www.youtube.com/embed/${videoKey}?autoplay=1&rel=0`; // autoplay para iniciar automaticamente, rel=0 para não mostrar vídeos relacionados de outros canais
+            // const youtubeWatchUrl = `https://www.youtube.com/watch?v=${videoKey}`;
+
+            // limpando a imagem
+            const imageMovie = document.getElementById('i_p')
+            imageMovie.style.display = 'none'
+
+            if(posterTrailer){
+                const iframe = document.createElement('iframe');
+                iframe.src = youtubeEmbedUrl;
+                iframe.width = "100%";
+                iframe.height = "100%";
+                iframe.setAttribute("frameborder", "0");
+                iframe.setAttribute("allowfullscreen", "");
+                iframe.setAttribute("allow", "autoplay; encrypted-media");
+                
+                posterTrailer.appendChild(iframe);
+
+            }
+
+            
+
+        } 
+
+        // for apply message for no have trailers 
+        if(trailerMovie === null){
+
+            const pInfo = document.createElement('p');
+            pInfo.textContent = 'Sorry no have Trailers';
+            pInfo.classList.add('info_no_trailers')
+            posterTrailer.appendChild(pInfo)
+
+        }
+
+
+    } catch (error) {
+        console.error('Erro ao buscar trailers do filme:', error);
+    }
+}
+
